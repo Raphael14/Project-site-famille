657cd652546ef5671a1937f3525856e8a5d2875f
Raphael Lequertier <raphaellequertier14@gmail.com>
2017-10-29 18:52:02 +0100

    Ajout
    Ajout dossier

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Dream-familly</title>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/script.js"></script>
+
+</head>
+<body>
+	<div class="test">
+		
+		<h1>test de la font</h1>
+	</div>
+</body>
+</html>
