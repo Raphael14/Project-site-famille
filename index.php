b7d99ff7ae41e8ed318287201412597d62bce30e
Raphael Lequertier <raphaellequertier14@gmail.com>
2017-10-31 18:42:06 +0100

    V1 de la page d'acceuil
    Modif par rapport aux pds

--- index.php
+++ index.php
@@ -2,16 +2,41 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Dream-familly</title>
+	<title>Dream Family</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="css/style.css">
 	<script src="js/script.js"></script>
 
 </head>
 <body>
-	<div class="test">
-		
-		<h1>test de la font</h1>
+	<div class="container-fluid">
+				
+	    <div class="row-fluid">
+			
+				<div id="nuage1"><img src="image/nuage1.png" alt="Nuage"></div>
+				<div id="nuage2"><img src="image/nuage2.png" alt="Nuage"></div>
+				<div id="nuage3"><img src="image/nuage3.png" alt="Nuage"></div>
+				<div id="nuage4"><img src="image/nuage4.png" alt="Nuage"></div>
+				
+			
+	        <div class="span3 centering text-center border border-info">	        	
+	            <div class="titre">
+	            	<h1>Dream-Family</h1>
+	            </div>
+	            <form action="acceil.php" method="post">
+	            	<input class="form-control user" type="text" name="username" placeholder="Username">
+	            	<input class="form-control password" type="text" name="password" placeholder="Password">
+	            	<button type="button" class="btn btn-success text-center ">Entrer</button><br>
+	            	<a href="#" class="lost">Lost Your Password ?</a>
+
+	            </form>
+	        </div>
+	    </div>
 	</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
 </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+setTimeout("animation()",1000);
+
+
+console.log('tiens salut');
+});
+function animation(){
+nuage1();
+nuage2();
+nuage3();
+nuage4();
+
+}
+
+
+
+
+
+
+
+
+
+function nuage1(){
+$("#nuage1").animate({left:"2500px"},150000).animate({left:"-200px"},0)
+setTimeout("nuage1()",1000);
+}
+
+function nuage2(){
+$("#nuage2").animate({left:"2500px"},40000).animate({left:"-200px"},0)
+setTimeout("nuage2()",1000);
+}
+function nuage3(){
+$("#nuage3").animate({left:"2500px"},120000).animate({left:"-200px"},0)
+setTimeout("nuage3()",1000);
+}function nuage4(){
+$("#nuage4").animate({left:"2500px"},75000).animate({left:"-200px"},0)
+setTimeout("nuage4()",1000);
+}
