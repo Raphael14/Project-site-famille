5d2b016c7849a52a1a5e030af1207f9d5c832c8d
Raphael Lequertier <raphaellequertier14@gmail.com>
2017-10-31 18:47:05 +0100

    Signed-off-by: Raphael Lequertier <raphaellequertier14@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,30 +1,16 @@
 $(document).ready(function(){
 setTimeout("animation()",1000);
-
-
-console.log('tiens salut');
 });
 function animation(){
 nuage1();
 nuage2();
 nuage3();
 nuage4();
-
 }
-
-
-
-
-
-
-
-
-
 function nuage1(){
 $("#nuage1").animate({left:"2500px"},150000).animate({left:"-200px"},0)
 setTimeout("nuage1()",1000);
 }
-
 function nuage2(){
 $("#nuage2").animate({left:"2500px"},40000).animate({left:"-200px"},0)
 setTimeout("nuage2()",1000);
